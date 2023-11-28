520478e09beaefadb566668d24512a99ed65c970
Xuan <leeyitsuen0617@gmail.com>
2023-11-28 11:46:53 +0800

    modify

--- data.py
+++ data.py
@@ -75,7 +75,7 @@ def __download_credit_data() -> csv:
                 f"https://bas.nccc.com.tw/nccc-nop/OpenAPI/C01/sexconsumption/{B}/{A}"
             )
             response_sex = requests.request("GET", sex_url)
-            if len(response_sex.text) == 0:
+            if len(response_sex.text) == 0:   #若csv無資料則繼續,不下載
                 continue
             with open(f"./datasource/sex/sex{B}_{A}.csv", "wb") as file:
                 file.write(response_sex.content)
@@ -133,25 +133,30 @@ def __download_credit_data() -> csv:
 
     # ---------合併csv---------#
     for item in DataType:
-        path = f"./datasource/{item}/"
+        path = f"./datasource/{item}/"  #要合併資料的路徑位置
+        #取得指定路徑下所有的csv
         csv_files = [file for file in os.listdir(path) if file.endswith(".csv")]
+        #建立空dataframe儲存合併資料
         merged_data = pd.DataFrame()
+        #讀取csv資料內容合併到merged_data
         for file in csv_files:
-            file_path = os.path.join(path, file)
-            data = pd.read_csv(file_path)
-            merged_data = pd.concat([merged_data, data], ignore_index=True)
-        merged_data.drop_duplicates(inplace=True)
-        merged_data.to_csv(f"{item}.csv", index=False)
+            file_path = os.path.join(path, file)  #將路徑(path)與file連接,建立完整路徑位置
+            data = pd.read_csv(file_path)  #讀取csv內容,並儲存在dataframe(data)中
+            #pd.concat->將data與merged_data連接, ignore_index=True->忽略原始的index欄位(索引值),改採用合併後自動產生的index(索引值)
+            merged_data = pd.concat([merged_data, data], ignore_index=True) 
+        merged_data.drop_duplicates(inplace=True)  #在合併前去除重複的值
+        merged_data.to_csv(f"{item}.csv", index=False)  #將merged_data寫入csv, index=False->不寫入dataframe的index(索引值)
         print(f"{item}.csv建立成功")
 
         with open(f"./{item}.csv", "r", encoding="UTF-8") as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            fieldnames = csv_reader.fieldnames
+            csv_reader = csv.DictReader(csv_file)  #使用DictReader+csv_reader.fieldnames->傳回csv的欄位名稱
+            fieldnames = csv_reader.fieldnames  #取得欄位名稱
 
-            with open(f"./{item}_trans.csv", "w", encoding="utf-8", newline="") as file:
-                new_fieldnames = ["年", "月"] + fieldnames[1:]
+            #將格式轉換完畢的資料內容寫入csv中
+            with open(f"./{item}_trans.csv", "w", encoding="utf-8", newline="") as file:  
+                new_fieldnames = ["年", "月"] + fieldnames[1:]  #將年,月寫在欄位最前面, fieldnames[1:]->取得從2到最後的欄位名稱(原始fieldnames中年月是在第一個,所以從第二個開始)
                 csv_writer = csv.DictWriter(file, fieldnames=new_fieldnames)
-                csv_writer.writeheader()
+                csv_writer.writeheader()  #寫入第一列的欄位名稱
 
                 for row in csv_reader:
                     #---找不到對應的值，則保持原本的值---#
@@ -160,16 +165,17 @@ def __download_credit_data() -> csv:
                     if "性別" in fieldnames:
                         row["性別"] = sex_code.get(row["性別"], row["性別"])
 
-                    year = row["年月"][:4]
-                    month = row["年月"][4:]
+                    #將原資料中的年月,拆開成年 月,ex:201401
+                    year = row["年月"][:4]  #取出值,由開頭到3, 0-3, 2014
+                    month = row["年月"][4:] #取出值,由4到最後, 4-5, 01
                     new_row = {"年": year, "月": month, "地區": row["地區"]}
 
-                    if "性別" in fieldnames:
+                    if "性別" in fieldnames:  #找尋csv中有性別欄位的資料
                         new_row["性別"] = row["性別"]
 
-                    new_row.update(row)
-                    del new_row["年月"]
-                    csv_writer.writerow(new_row)
+                    new_row.update(row)  #將dictionary(new_row)更新並合併到另一個dictionary(row)
+                    del new_row["年月"]  #刪除原csv裡的年月
+                    csv_writer.writerow(new_row)  #將new_row資料寫入
 
                 print(f"{item}_trans.csv建立成功")
 
@@ -183,8 +189,8 @@ def csv_to_database() -> None:
         file = f"./{item}_trans.csv"
         df = pd.read_csv(file)
 
-        df.rename(columns={"信用卡交易金額[新台幣]": "信用卡金額"}, inplace=True)
-        df.to_sql(item, conn, if_exists="replace", index=False)
+        df.rename(columns={"信用卡交易金額[新台幣]": "信用卡金額"}, inplace=True)  #inplace=True->直接修改原始dataframe
+        df.to_sql(item, conn, if_exists="replace", index=False)  #寫入資料庫中,if_exists="replace"->若table已經存在將其替換, index=False->不將dataframe的索引值寫入
 
     conn.close()
 
