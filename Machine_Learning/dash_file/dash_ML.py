c134c87c0d80073c12905b4fde5b51af5908186b
HH <wlhs1726@gmail.com>
2024-01-29 22:05:41 +0800

    modify

--- Machine_Learning/dash_file/dash_ML.py
+++ Machine_Learning/dash_file/dash_ML.py
@@ -1,7 +1,6 @@
-from dash import Dash, html, dash_table, callback, Input, Output, dcc
+from dash import Dash, html, callback, Input, Output, dcc
 import dash_bootstrap_components as dbc
 import pandas as pd
-import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
